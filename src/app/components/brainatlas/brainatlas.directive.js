27e9ca7194d415546344f2130651c86a3973cfb4
Marko Grešak <marko@gresak.io>
2015-07-15 17:36:49 +0200

    add comments and remove log messages

--- src/app/components/brainatlas/brainatlas.directive.js
+++ src/app/components/brainatlas/brainatlas.directive.js
@@ -28,15 +28,23 @@
                 zoomStepDuration: 0.05,
                 zoomToFitZoomLevelFactor: 1.0
             };
+            // According to Panzoom docs, the model should be declared as empty object.
             $scope.panzoomModel = {};
 
+            /**
+             * Set `imageIndex` as active image.
+             *
+             * @param {Number} imageIndex Index of next active image.
+             */
             $scope.setImage = function (imageIndex) {
-                console.log('set', imageIndex);
+                // Change `src` of displayed image.
                 $element.find('.display-img').attr('src', $scope.imageUrls[imageIndex]);
+                // Toggle active class.
                 $element.find('.brainatlas-thumbnail-container li.active').removeClass('active');
                 $element.find('.brainatlas-thumbnail-container li').eq(imageIndex).addClass('active');
             };
 
+            // Call `setImage` with index 0 when directive renders.
             $timeout(function () { $scope.setImage(0); }, 0);
         }
     }
