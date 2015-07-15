b695abf64ef7b46d13ce886fd9905feaada3d7ce
Marko Grešak <marko@gresak.io>
2015-07-15 16:18:07 +0200

    change randomInt to _.random

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,16 +1,9 @@
 (function () {
     'use strict';
 
-
     angular.module('brainatlasGallery').controller('MainController', function ($scope) {
-        var randomInt = function (min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        };
-
-        var randomPlaceholder = function () {
-            return 'http://placehold.it/' + randomInt(100, 600) + 'x' + randomInt(100, 600);
-        };
-
-        $scope.imageUrls = _.range(10).map(randomPlaceholder);
+        $scope.imageUrls = _.range(10).map(function () {
+            return 'http://placehold.it/' + _.random(100, 600) + 'x' + _.random(100, 600);
+        });
     });
 })();
