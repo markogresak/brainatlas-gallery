3dcbee82d8007c887282823edc360fc92be80e05
Marko Grešak <marko@gresak.io>
2015-07-15 15:05:59 +0200

    define array of 10 random $scope.imageUrls

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,7 +1,17 @@
 (function () {
     'use strict';
 
-    angular.module('brainatlasGallery').controller('MainController', function () {
 
+    angular.module('brainatlasGallery').controller('MainController', function ($scope) {
+        var randomInt = function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+
+        var randomPlaceholder = function () {
+            return 'http://placehold.it/' + randomInt(100, 600) + 'x' + randomInt(100, 600);
+        };
+
+        $scope.imageUrls = _.range(10).map(randomPlaceholder);
+        console.log('main: ', $scope.imageUrls);
     });
 })();
