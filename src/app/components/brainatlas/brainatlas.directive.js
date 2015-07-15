de6b0b2c23796bdbe2fe1ea303c88da355a4aef9
Marko Grešak <marko@gresak.io>
2015-07-15 17:52:11 +0200

    update code to set active class using ng-class

--- src/app/components/brainatlas/brainatlas.directive.js
+++ src/app/components/brainatlas/brainatlas.directive.js
@@ -32,20 +32,17 @@
             $scope.panzoomModel = {};
 
             /**
-             * Set `imageIndex` as active image.
+             * Update displayed image.
              *
-             * @param {Number} imageIndex Index of next active image.
+             * @param {String} imageUrl Url (`src`) to new image.
              */
-            $scope.setImage = function (imageIndex) {
-                // Change `src` of displayed image.
-                $element.find('.display-img').attr('src', $scope.imageUrls[imageIndex]);
-                // Toggle active class.
-                $element.find('.brainatlas-thumbnail-container li.active').removeClass('active');
-                $element.find('.brainatlas-thumbnail-container li').eq(imageIndex).addClass('active');
+            $scope.setImage = function (imageUrl) {
+                $scope.displayedUrl = imageUrl;
             };
 
-            // Call `setImage` with index 0 when directive renders.
-            $timeout(function () { $scope.setImage(0); }, 0);
+            // Call `setImage` with first image url (called when directive renders).
+            $timeout(function () { $scope.setImage($scope.imageUrls[0]); }, 0);
+
         }
     }
 
