de03d58c73a23314cce69983f279da28484010d4
Marko Grešak <marko@gresak.io>
2015-07-16 11:37:00 +0200

    add test to check if brainatlas directive compiles

--- src/app/components/brainatlas/brainatlas.spec.js
+++ src/app/components/brainatlas/brainatlas.spec.js
@@ -3,10 +3,31 @@
 
     describe('brainatlas', function () {
 
-        beforeEach(module('brainatlasGallery'));
+        var imageUrls, el, scope;
 
-        it('should pass', inject(function ($controller) {
-            expect(true).toBeTruthy();
+        beforeEach(function () {
+            imageUrls = _.range(10).map(function () {
+                return 'http://placehold.it/' + _.random(100, 600) + 'x' + _.random(100, 600);
+            });
+        });
+
+        beforeEach(module('brainatlasGallery'));
+        beforeEach(inject(function ($compile, $rootScope) {
+            el = angular.element(
+                '<div class="widget-wrapper">' +
+                    '<brainatlas image-urls="imageUrls"></brainatlas>' +
+                '</div>'
+            );
+            scope = $rootScope;
+            $compile(el)(scope);
+            scope.$apply(function () {
+                scope.imageUrls = imageUrls;
+            });
         }));
+
+        it('should be compiled', function () {
+            expect(el.html()).not.toEqual(null);
+        });
+
     });
 })();
