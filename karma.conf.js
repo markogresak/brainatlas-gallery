340d0d024d1197f8d8aef5656264a977241c21d6
Marko Grešak <marko@gresak.io>
2015-07-17 10:45:09 +0200

    update travis/karma config to add SauceLabs runner

--- karma.conf.js
+++ karma.conf.js
@@ -7,13 +7,13 @@ var _ = require('lodash');
 var wiredep = require('wiredep');
 
 function listFiles() {
-  var wiredepOptions = _.extend({}, conf.wiredep, {
-    dependencies: true,
-    devDependencies: true
-  });
+    var wiredepOptions = _.extend({}, conf.wiredep, {
+        dependencies: true,
+        devDependencies: true
+    });
 
-  return wiredep(wiredepOptions).js
-    .concat([
+    return wiredep(wiredepOptions).js
+        .concat([
       path.join(conf.paths.src, '/app/**/*.module.js'),
       path.join(conf.paths.src, '/app/**/*.js'),
       path.join(conf.paths.src, '/**/*.spec.js'),
@@ -22,56 +22,158 @@ function listFiles() {
     ]);
 }
 
-module.exports = function(config) {
+module.exports = function (config) {
 
-  var configuration = {
-    files: listFiles(),
+    var configuration = {
+        files: listFiles(),
 
-    singleRun: true,
+        singleRun: true,
 
-    autoWatch: false,
+        autoWatch: false,
 
-    frameworks: ['jasmine', 'angular-filesort'],
+        frameworks: ['jasmine', 'angular-filesort'],
 
-    angularFilesort: {
-      whitelist: [path.join(conf.paths.src, '/**/!(*.html|*.spec|*.mock).js')]
-    },
+        angularFilesort: {
+            whitelist: [path.join(conf.paths.src, '/**/!(*.html|*.spec|*.mock).js')]
+        },
 
-    ngHtml2JsPreprocessor: {
-      stripPrefix: 'src/',
-      moduleName: 'brainatlasGallery'
-    },
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'src/',
+            moduleName: 'brainatlasGallery'
+        },
 
-    browsers : ['PhantomJS', 'Chrome', 'Safari', 'Firefox'],
+        browsers: ['PhantomJS', 'Chrome', 'Safari', 'Firefox'],
 
-    plugins : [
+        plugins: [
       'karma-phantomjs-launcher',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-safari-launcher',
+      'karma-sauce-launcher',
       'karma-angular-filesort',
       'karma-jasmine',
       'karma-ng-html2js-preprocessor'
     ],
 
-    preprocessors: {
-      'src/**/*.html': ['ng-html2js']
-    }
-  };
-
-  // This block is needed to execute Chrome on Travis
-  // If you ever plan to use Chrome and Travis, you can keep it
-  // If not, you can safely remove it
-  // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
-  if(process.env.TRAVIS) {
-    configuration.customLaunchers = {
-      'chrome-travis-ci': {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
+        preprocessors: {
+            'src/**/*.html': ['ng-html2js']
+        }
     };
-    configuration.browsers = ['PhantomJS', 'Firefox', 'chrome-travis-ci'];
-  }
 
-  config.set(configuration);
+    // This block is needed to execute Chrome on Travis
+    // If you ever plan to use Chrome and Travis, you can keep it
+    // If not, you can safely remove it
+    // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
+    if (process.env.TRAVIS) {
+        // Check for SauceLabs information.
+        if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+            console.error('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
+            process.exit(1);
+        }
+
+        configuration.customLaunchers = {
+            sl_chrome_windows81: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'Windows 8.1'
+            },
+            sl_chrome_windows7: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'Windows 7'
+            },
+            sl_chrome_linux: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'linux'
+            },
+            sl_chrome_osx1010: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X Yosemite'
+            },
+            sl_chrome_osx1009: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X Mavericks'
+            },
+            sl_firefox_windows81: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'Windows 8.1'
+            },
+            sl_firefox_windows7: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'Windows 7'
+            },
+            sl_firefox_linux: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'linux'
+            },
+            sl_firefox_osx1010: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X Yosemite'
+            },
+            sl_firefox_osx1009: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X Mavericks'
+            },
+            sl_safari_osx1010: {
+                base: 'SauceLabs',
+                browserName: 'safari',
+                platform: 'OS X Yosemite'
+            },
+            sl_safari_osx1009: {
+                base: 'SauceLabs',
+                browserName: 'safari',
+                platform: 'OS X Mavericks'
+            },
+            sl_ie11_windows81: {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 8.1'
+            },
+            sl_ie11_windows7: {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 7'
+            },
+            sl_iphone_ios8_safari: {
+                base: 'SauceLabs',
+                browserName: 'iphone',
+                version: '8.2'
+            },
+            sl_ipad_ios8_safari: {
+                base: 'SauceLabs',
+                browserName: 'ipad',
+                version: '8.2'
+            },
+            sl_iphone_ios7_safari: {
+                base: 'SauceLabs',
+                browserName: 'iphone',
+                version: '7.1'
+            },
+            sl_ipad_ios7_safari: {
+                base: 'SauceLabs',
+                browserName: 'ipad',
+                version: '7.1'
+            }
+        };
+
+        configuration.sauceLabs = {
+            testName: 'Brainatlas unit tests',
+            connectOptions: {
+                tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+            }
+        };
+
+        configuration.reporters.push('saucelabs');
+        configuration.browsers = Object.keys(configuration.customLaunchers);
+    }
+
+    config.set(configuration);
 };
